4531ceff9a7414e5123ed7f06c2cecb74fdc52cb
Aaron He <aaronhe09@yahoo.com>
2023-05-30 20:59:56 +0000

    Moved main.js code over to data.js. Added a submit event listener to the form #6

--- js/data.js
+++ js/data.js
@@ -1,8 +1,31 @@
 /* exported data */
-
+const $photoUrl = document.querySelector('#photo-url');
+const $image = document.querySelector('.image');
+const $form = document.querySelector('form');
+const $title = document.querySelector('#title');
+const $notes = document.querySelector('#notes');
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+$photoUrl.addEventListener('input', function (e) {
+  $image.src = e.target.value;
+});
+
+$form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const object = {
+    title: $title.value,
+    photoUrl: $photoUrl.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId
+  };
+
+  data.nextEntryId += 1;
+  data.entries.push(object);
+  $image.src = 'images/placeholder-image-square.jpg';
+  $form.reset();
+});
--- js/main.js
+++
@@ -1,6 +0,0 @@
-const $photoUrl = document.querySelector('#photo-url');
-const $image = document.querySelector('.image');
-
-$photoUrl.addEventListener('input', function (e) {
-  $image.src = e.target.value;
-});
