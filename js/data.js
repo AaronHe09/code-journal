603eb676bfc9ccc6ff521ade8bcd423e46a0f184
Aaron He <aaronhe09@yahoo.com>
2023-05-30 22:10:27 +0000

    changed class attribute names and moved code to main.js

--- js/data.js
+++ js/data.js
@@ -1,9 +1,4 @@
 /* exported data */
-const $photoUrl = document.querySelector('#photo-url');
-const $image = document.querySelector('.image');
-const $form = document.querySelector('form');
-const $title = document.querySelector('#title');
-const $notes = document.querySelector('#notes');
 var data = {
   view: 'entry-form',
   entries: [],
@@ -11,28 +6,9 @@ var data = {
   nextEntryId: 1
 };
 
-$photoUrl.addEventListener('input', function (e) {
-  $image.src = e.target.value;
-});
-
-$form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const object = {
-    title: $title.value,
-    photoUrl: $photoUrl.value,
-    notes: $notes.value,
-    entryId: data.nextEntryId
-  };
-
-  data.nextEntryId += 1;
-  data.entries.push(object);
-  $image.src = 'images/placeholder-image-square.jpg';
-  $form.reset();
-});
-
 window.addEventListener('beforeunload', function () {
   const dataJSON = JSON.stringify(data);
-  this.localStorage.setItem('data', dataJSON);
+  localStorage.setItem('data', dataJSON);
 });
 
 if (localStorage.getItem('data') !== null) {
---
+++ js/main.js
@@ -0,0 +1,24 @@
+const $photoUrl = document.querySelector('#photo-url');
+const $image = document.querySelector('.image');
+const $form = document.querySelector('form');
+const $title = document.querySelector('#title');
+const $notes = document.querySelector('#notes');
+
+$photoUrl.addEventListener('input', function (e) {
+  $image.src = e.target.value;
+});
+
+$form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const object = {
+    title: $title.value,
+    photoUrl: $photoUrl.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId
+  };
+
+  data.nextEntryId += 1;
+  data.entries.unshift(object);
+  $image.src = 'images/placeholder-image-square.jpg';
+  $form.reset();
+});
